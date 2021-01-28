2928beb6d43c2fb61363b033d4f237b0390374a5
copygirl <copygirl@mcft.net>
2021-01-28 11:16:32 +0100

    Only apply Patch to newly generated worlds

--- src/SkyChunkSystem.cs
+++ src/SkyChunkSystem.cs
@@ -20,7 +20,23 @@ namespace SkyChunk
 		public Harmony Harmony { get; } = new(MOD_ID);
 
 		public override void StartServerSide(ICoreServerAPI api)
-			=> Harmony.PatchAll();
+		{
+			Harmony.PatchAll();
+
+			// Set a flag on world creation so this mod only ever affects
+			// worlds created when the mod is present, never existing ones.
+
+			// NOTE: As it turns out, this event is fired before any mods are loaded.
+			//       So, I guess we'll have to work around this issue a little bit ...
+			// api.Event.SaveGameCreated += () =>
+			// 	api.WorldManager.SaveGame.WorldConfiguration.SetBool(
+			// 		MOD_ID + ":enabled", GenTerra_OnChunkColumnGen_Patch.Enabled = true);
+
+			api.Event.SaveGameLoaded += () => {
+				var config = api.WorldManager.SaveGame.WorldConfiguration;
+				GenTerra_OnChunkColumnGen_Patch.Enabled = config.GetBool(MOD_ID + ":enabled");
+			};
+		}
 
 		public override void Dispose()
 			=> Harmony.UnpatchAll(MOD_ID);
@@ -29,7 +45,8 @@ namespace SkyChunk
 	[HarmonyPatch(typeof(GenTerra), "OnChunkColumnGen")]
 	class GenTerra_OnChunkColumnGen_Patch
 	{
-		static Random Random { get; } = new();
+		private static Random Random { get; } = new();
+		public static bool Enabled { get; set; }
 
 		static bool Prefix(ICoreServerAPI ___api, IServerChunk[] chunks, int chunkX, int chunkZ, ITreeAttribute chunkGenParams)
 		{
@@ -37,8 +54,14 @@ namespace SkyChunk
 
 			// Always generate the 4 spawn chunks around the default spawn position.
 			if ((chunkX / 2 == world.MapSizeX / world.ChunkSize / 4) &&
-			    (chunkZ / 2 == world.MapSizeZ / world.ChunkSize / 4))
+			    (chunkZ / 2 == world.MapSizeZ / world.ChunkSize / 4)) {
+				// This is our previously mentioned workaround.
+				world.SaveGame.WorldConfiguration.SetBool(
+					SkyChunkSystem.MOD_ID + ":enabled", Enabled = true);
 				return true;
+			}
+
+			if (!Enabled) return true;
 
 			// Seeding the Random like this seems to generate patterns, so non-deterministic it is!
 			// var seed = (int)world.Seed ^ chunkX ^ (chunkZ << 16 | chunkZ >> 16);
