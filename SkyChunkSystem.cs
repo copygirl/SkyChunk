07ea1b10eac7230f8a264aeddeeab67429154abe
copygirl <copygirl@mcft.net>
2021-01-28 15:02:27 +0100

    Use ISaveGame.IsNew

--- SkyChunkSystem.cs
+++ SkyChunkSystem.cs
@@ -21,20 +21,15 @@ namespace SkyChunk
 
 		public override void StartServerSide(ICoreServerAPI api)
 		{
-			Harmony.PatchAll();
-
-			// Set a flag on world creation so this mod only ever affects
-			// worlds created when the mod is present, never existing ones.
-
-			// NOTE: As it turns out, this event is fired before any mods are loaded.
-			//       So, I guess we'll have to work around this issue a little bit ...
-			// api.Event.SaveGameCreated += () =>
-			// 	api.WorldManager.SaveGame.WorldConfiguration.SetBool(
-			// 		MOD_ID + ":enabled", GenTerra_OnChunkColumnGen_Patch.Enabled = true);
-
 			api.Event.SaveGameLoaded += () => {
 				var config = api.WorldManager.SaveGame.WorldConfiguration;
-				GenTerra_OnChunkColumnGen_Patch.Enabled = config.GetBool(MOD_ID + ":enabled");
+				// Set a flag on world creation so this mod only ever affects
+				// worlds created when the mod is present, never existing ones.
+				if (api.WorldManager.SaveGame.IsNew) config.SetBool(MOD_ID + ":enabled", true);
+				else if (!config.GetBool(MOD_ID + ":enabled")) return;
+
+				// Only apply patch when world is new or flag was set.
+				Harmony.PatchAll();
 			};
 		}
 
@@ -45,23 +40,15 @@ namespace SkyChunk
 	[HarmonyPatch(typeof(GenTerra), "OnChunkColumnGen")]
 	class GenTerra_OnChunkColumnGen_Patch
 	{
-		private static Random Random { get; } = new();
-		public static bool Enabled { get; set; }
+		static Random Random { get; } = new();
 
 		static bool Prefix(ICoreServerAPI ___api, IServerChunk[] chunks, int chunkX, int chunkZ, ITreeAttribute chunkGenParams)
 		{
 			var world = ___api.WorldManager;
-
 			// Always generate the 4 spawn chunks around the default spawn position.
 			if ((chunkX / 2 == world.MapSizeX / world.ChunkSize / 4) &&
-			    (chunkZ / 2 == world.MapSizeZ / world.ChunkSize / 4)) {
-				// This is our previously mentioned workaround.
-				world.SaveGame.WorldConfiguration.SetBool(
-					SkyChunkSystem.MOD_ID + ":enabled", Enabled = true);
+			    (chunkZ / 2 == world.MapSizeZ / world.ChunkSize / 4))
 				return true;
-			}
-
-			if (!Enabled) return true;
 
 			// Seeding the Random like this seems to generate patterns, so non-deterministic it is!
 			// var seed = (int)world.Seed ^ chunkX ^ (chunkZ << 16 | chunkZ >> 16);
